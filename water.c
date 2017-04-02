e77c5ff22be58d99bbdf58453a5063382b9a7d69
Ivan Leo <ivanleo97@gmail.com>
2017-04-02 14:01:34 +0800

    Update water.c

--- water.c
+++ water.c
@@ -1,3 +1,5 @@
+-------- Objective C ----------
+
 #include <stdio.h>
 #include <cs50.h>
 
@@ -18,3 +20,15 @@ int main (void)
     printf("Bottles: %i\n", minutes*12);
           
 }
+
+-------- Python ---------
+   
+print('Minutes: ', end='');
+x = int(input());
+
+while(x<0):
+  print("That is an invalid input")
+  print('Minutes: ', end='');
+  x = int(input());
+
+print("Bottles: " + str(x*12));
