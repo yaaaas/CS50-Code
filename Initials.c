7a5a975be5d996d400ce8937bbab5955ee4649c3
Ivan Leo <ivanleo97@gmail.com>
2017-04-03 09:59:33 +0800

    Create Initials.c

---
+++ Initials.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void){
+    
+    printf("What's your Name?\n");
+    string name = get_string();
+    printf("%c",toupper(name[0]));
+    
+    for(int i = 0; i < strlen(name) ; i++){
+        if(name[i] == ' '){
+            printf("%c",toupper(name[i+1]));
+        }
+            
+        
+    }
+    
+    print("\n");
+}
